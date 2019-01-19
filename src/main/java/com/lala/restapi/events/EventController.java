f35a7161feef935648ad01984eac1b26ae0de890
ryan <whuk84@gmail.com>
2019-01-19 19:24:55 +0900

    add get queryEvents

--- src/main/java/com/lala/restapi/events/EventController.java
+++ src/main/java/com/lala/restapi/events/EventController.java
@@ -2,12 +2,16 @@ package com.lala.restapi.events;
 
 import com.lala.restapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +61,15 @@ public class EventController {
         eventResource.add(new Link("docs/com.lala.restapi.index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
     }
+
+    @GetMapping
+    public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        var pagedResources = assembler.toResource(page, e -> new EventResource(e));
+        pagedResources.add(new Link("docs/com.lala.restapi.index.html#resources-events-list").withRel("profile"));
+        return ResponseEntity.ok(pagedResources);
+    }
+
 
     private ResponseEntity getResponseEntity(Errors errors) {
         return ResponseEntity.badRequest().body(new ErrorsResource(errors));
